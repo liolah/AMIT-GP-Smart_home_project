70c4600350b3a5cf159431d09acb6181cbfd13f7
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-01 19:02:30 +0200

    I2C update 2

--- Smart_Home/Smart_Home/MCAL/I2C/I2C.c
+++ Smart_Home/Smart_Home/MCAL/I2C/I2C.c
@@ -11,89 +11,43 @@ void I2C_init(void) {
   TWBR = BITRATE(TWSR = 0x00);	// Get bit rate register value by formula 
   }
 
-EN_I2CMasterStatusCode_t I2C_start(uint8_t address) {
+EN_I2CMasterStatusCode_t I2C_start() {
   // Generate start bit
   //? I tried setting each bit individually but it didn't work. For some reason the bits must be set at the same time. Probably because clearing TWINT starts the task? I don't know. 
   TWCR = (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWSTA);
-  // set_bit(TWCR, TWEN); // Enable the TWI interface
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWINT) == 0);	// Wait until TWI finish its current job
-  if ((TWSR & 0xF8) != 0x08)						 // check if the start condition was successful
-    return START_TRANSMISSION_FAILED; // If not return error
-  TWDR = address;
-  TWCR = (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
-  while (read_bit(TWCR, TWINT) == 0);
-  if ((TWSR & 0xF8) == 0x18)
-    return SLA_W_SENT_ACK_RECEIVED;
-  if ((TWSR & 0xF8) == 0x20)
-    return SLA_W_SENT_NACK_RECEIVED;
-  return ERROR;
+  return getCurrentStatus();
   }
 
-EN_I2CMasterStatusCode_t I2C_repeatedStart(uint8_t read_address) {
+EN_I2CMasterStatusCode_t I2C_repeatedStart() {   // Same as start, just a different name
   TWCR = (1 << TWSTA) | (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWSTA);
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWINT) == 0);
-  if ((TWSR & 0xF8) != 0x10)						 // check if the repeated start condition was successful
-    return REPEATED_START_TRANSMISSION_FAILED; // If not return error
-  TWDR = read_address;
-  TWCR = (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
-  while (read_bit(TWCR, TWINT) == 0);
-  if ((TWSR & 0xF8) == 0x40)
-    return SLA_R_SENT_ACK_RECEIVED;
-  if ((TWSR & 0xF8) == 0x48)
-    return SLA_R_SENT_NACK_RECEIVED;
-  return ERROR;
+  return getCurrentStatus();
   }
 
 EN_I2CMasterStatusCode_t I2C_write(uint8_t data) {
   TWDR = data;
   TWCR = (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWINT) == 0);
-  if ((TWSR & 0xF8) == 0x28)
-    return DATA_SENT_ACK_RECEIVED;
-  else if ((TWSR & 0xF8) == 0x30)
-    return DATA_SENT_NACK_RECEIVED;
-  return ERROR;
+  return getCurrentStatus();
   }
 
 EN_I2CMasterStatusCode_t I2C_read_Ack(uint8_t* data) {
   TWCR = (1 << TWEA) | (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEA);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWINT) == 0);
   *data = TWDR;
-  if ((TWSR & 0xF8) == 0x50)
-    return DATA_RECEIVED_ACK_SENT;
-  return ERROR;
+  return getCurrentStatus();
   }
 
 EN_I2CMasterStatusCode_t I2C_read_Nack(uint8_t* data) {
   TWCR = (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWINT) == 0);
   *data = TWDR;
-  if ((TWSR & 0xF8) == 0x58)
-    return DATA_RECEIVED_NACK_SENT;
-  return ERROR;
+  return getCurrentStatus();
   }
 
 void I2C_stop(void) {
   TWCR = (1 << TWSTO) | (1 << TWEN) | (1 << TWINT);
-  // set_bit(TWCR, TWSTO);
-  // set_bit(TWCR, TWEN);
-  // set_bit(TWCR, TWINT);
   while (read_bit(TWCR, TWSTO));	// Wait until stop condition finishes
   }
 
