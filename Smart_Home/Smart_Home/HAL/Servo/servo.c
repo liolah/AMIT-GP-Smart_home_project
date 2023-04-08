5437ebad1ac23d82e93516144843f48bb3e25178
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-08 21:43:47 +0200

    Servo v0.3

--- Smart_Home/Smart_Home/HAL/Servo/servo.c
+++ Smart_Home/Smart_Home/HAL/Servo/servo.c
@@ -7,10 +7,11 @@
 
 #include "servo.h"
 
-// Initialized the servo
+ // Initialized the servo
 void servo_init(){
-   
-}
+  DIO_init(SERVO_PIN, SERVO_PORT, OUT);
+  PWM_init(SERVO_PWM_PIN, 0, PWM_PHASE_CORRECT);
+  }
 
 // Moves the servo to a specific angle (relative to the 0 angle position, not the previous one).
 void servo_move_to_angle(uint8_t angle){
--- Smart_Home/Smart_Home/HAL/Servo/servo.h
+++ Smart_Home/Smart_Home/HAL/Servo/servo.h
@@ -3,8 +3,15 @@
  *
  * Created: 3/26/2023 5:08:55 PM
  *  Author: hesha
- */ 
+ */
 
+
+ //? This a driver fro SG90 9g Micro Servo
+//^ The servo takes a PWM signal with 50Hz frequency as input (Pulse width, aka periodic time, is 20mS).
+//^ The servo angle is determined by the duration of the high pulse (duty cycle). 
+//^ A PWM signal with high pulse duration of * 1mS * will make the servo move to * 0 * angle position
+//^ A PWM signal with high pulse duration of * 1.5mS * will make the servo move to * 90 * angle position
+//^ A PWM signal with high pulse duration of * 2mS * will make the servo move to * 180 * angle position
 
 #ifndef SERVO_H_
 #define SERVO_H_
@@ -16,6 +23,7 @@
 #define SERVO_MIN_CONTROL_PULSE_WIDTH_uS 2000
 
 #define SERVO_PORT PORT_D
+#define SERVO_PWM_PIN  OC_1A
 #define SERVO_PIN  5
 
 // Initialized the servo
