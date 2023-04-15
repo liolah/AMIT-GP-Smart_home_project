d85ebef6db3ea6ba17c5a8f5e71cdbd95c8039a7
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-15 12:31:02 +0200

    Door service finished

--- Smart_Home/Smart_Home/Services/Door/door.c
+++ Smart_Home/Smart_Home/Services/Door/door.c
@@ -3,4 +3,21 @@
  *
  * Created: 4/14/2023 6:27:13 AM
  *  Author: hesha
- */ 
+ */
+
+#include "door.h"
+
+ // Initialize the servo for the door
+void Door_init(void) {
+  servo_init();
+  }
+
+// Open the door by moving the servo to the open angle
+void Door_open(void) {
+  Servo_move_to_angle(DOOR_OPEN_ANGLE);
+  }
+
+// Close the door by moving the servo to the close angle
+void Door_close(void) {
+  Servo_move_to_angle(DOOR_CLOSE_ANGLE);
+  }
--- Smart_Home/Smart_Home/Services/Door/door.h
+++ Smart_Home/Smart_Home/Services/Door/door.h
@@ -9,8 +9,18 @@
 #ifndef DOOR_H_
 #define DOOR_H_
 
+#include "../../HAL/Servo/servo.h"
 
+#define DOOR_OPEN_ANGLE   180
+#define DOOR_CLOSE_ANGLE  0
 
+// Initialize the servo for the door
+void Door_init(void);
 
+// Open the door by moving the servo to the open angle
+void Door_open(void);
+
+// Close the door by moving the servo to the close angle
+void Door_close(void);
 
 #endif /* DOOR_H_ */
