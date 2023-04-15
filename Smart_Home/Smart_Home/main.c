8637a204f3122602b3b6d2a4c07aa27fe4f1df23
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-15 09:05:11 +0200

    ADC auto trig test

--- Smart_Home/Smart_Home/main.c
+++ Smart_Home/Smart_Home/main.c
@@ -19,24 +19,69 @@
 #include "Services/User/user.h"
 #include "Services/Remote_access/remote.h"
 
-// #include "Application/app.h"
+u16 temperature;
+ // #include "Application/app.h"
+ISR(ADC_vect) {
+	temperature = (u16)ADCL;
+	temperature += ((u16)ADCH << 8);
+	DIO_toggle(1, 'B');
+	// set_bit(TIFR, TOV1);
+	set_bit(TIFR, OCF0);
+	}
 
+// ISR(TIMER1_OVF_vect) {
+// 	// PORTB = PORTB - 1;
+// 	DIO_toggle(7, 'D');
+// 	}
+ISR(TIMER0_COMP_vect) {
+	// PORTB = PORTB - 1;
+	DIO_toggle(7, 'D');
+	}
 int main(void) {
-	PWM_init(OC_1A, 0.5, PWM_PHASE_CORRECT);
-	Timer_start(TIMER_1, 8);
-	Timer_CTC_init(TIMER_0, 230);
-	Timer_start(TIMER_0, 1024);
-	DDRB = 0xff;
-	AC_init();
+	// PWM_init(OC_1A, 0.5, PWM_PHASE_CORRECT);
+	// DIO_init(5, PORT_D, OUT);
+	// Timer_start(TIMER_1, 8);
+	sei();
+	DIO_init(7, PORT_A, IN);
+	DIO_init(7, PORT_D, OUT);
+	
+	set_bit(TCCR0, WGM01);
+	OCR0 = 255;
+	set_bit(TCCR0, CS00);
+	set_bit(TCCR0, CS02);
+	// set_bit(TCCR1B, CS10);
+
+	// set_bit(TIMSK, TOIE1);
+	set_bit(TIMSK, OCIE0);
+	
+	set_bit(ADMUX, REFS0);
+	set_bit(ADMUX, REFS1);
+
+	set_bit(ADCSRA, ADPS0);
+	set_bit(ADCSRA, ADPS1);
+	set_bit(ADCSRA, ADPS2);
+	// Enable the auto trigger mode
+	set_bit(ADCSRA, ADATE);
+	
+	SFIOR |= (1 << ADTS0) | (1 << ADTS1);
+	// SFIOR |= (1 << ADTS2) | (1 << ADTS1);
+	// set_bit(SFIOR, ADTS2);
+
+	ADMUX = (ADMUX & 0xE0) | 7;
+
+	set_bit(ADCSRA, ADIE);
+	set_bit(ADCSRA, ADEN);
+
+	DIO_init(1, 'B', OUT);
+	// AC_init();
 	// Remote_init(9600);
 	// DIO_init(3, PORT_D, OUT);
 	// INT0_init(RISING_EDGE_INTERRUPT_REQUEST);
 	// UART_RXC_INT_init();
-	sei();
 	// App_init();
 
 	while (1) {
-
+		
 		// App_start();
 		
 		}
