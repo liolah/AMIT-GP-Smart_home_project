49e40deab0b1f13a63c17ae55c17e654f92bdb4a
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-15 14:05:27 +0200

    lights finalized

--- Smart_Home/Smart_Home/Services/Lights/lights.c
+++ Smart_Home/Smart_Home/Services/Lights/lights.c
@@ -3,4 +3,96 @@
  *
  * Created: 4/14/2023 6:25:38 AM
  *  Author: hesha
- */ 
+ */
+
+#include "lights.h"
+
+ // Initializes all six lamps
+void Lamps_init(void) {
+  LED_init(LAMP_1_PORT, LAMP_1_PIN);
+  LED_init(LAMP_2_PORT, LAMP_2_PIN);
+  LED_init(LAMP_3_PORT, LAMP_3_PIN);
+  LED_init(LAMP_4_PORT, LAMP_4_PIN);
+  LED_init(LAMP_5_PORT, LAMP_5_PIN);
+  LED_init(LAMP_6_DIMMABLE_PORT, LAMP_6_DIMMABLE_PIN);
+  // Initialize the pwm pin which lamp6 is on for brightness control
+  PWM_init(LAMP_6_DIMMABLE_PWM_PIN, 1, PWM_FAST);
+  Timer_start(TIMER_2, 1);
+  }
+
+// Turn on a lamp
+void Lamp_on(u8 lampNumber) {
+  switch (lampNumber) {
+      case LAMP_1:
+        LED_on(LAMP_1_PORT, LAMP_1_PIN);
+        break;
+      case LAMP_2:
+        LED_on(LAMP_2_PORT, LAMP_2_PIN);
+        break;
+      case LAMP_3:
+        LED_on(LAMP_3_PORT, LAMP_3_PIN);
+        break;
+      case LAMP_4:
+        LED_on(LAMP_4_PORT, LAMP_4_PIN);
+        break;
+      case LAMP_5:
+        LED_on(LAMP_5_PORT, LAMP_5_PIN);
+        break;
+      case LAMP_6:
+        LED_on(LAMP_6_DIMMABLE_PORT, LAMP_6_DIMMABLE_PIN);
+        break;
+    }
+  }
+
+// Turn off a lamp
+void Lamp_off(u8 lampNumber) {
+  switch (lampNumber) {
+      case LAMP_1:
+        LED_off(LAMP_1_PORT, LAMP_1_PIN);
+        break;
+      case LAMP_2:
+        LED_off(LAMP_2_PORT, LAMP_2_PIN);
+        break;
+      case LAMP_3:
+        LED_off(LAMP_3_PORT, LAMP_3_PIN);
+        break;
+      case LAMP_4:
+        LED_off(LAMP_4_PORT, LAMP_4_PIN);
+        break;
+      case LAMP_5:
+        LED_off(LAMP_5_PORT, LAMP_5_PIN);
+        break;
+      case LAMP_6:
+        LED_off(LAMP_6_DIMMABLE_PORT, LAMP_6_DIMMABLE_PIN);
+        break;
+    }
+  }
+
+// Toggle a lamp
+void Lamp_toggle(u8 lampNumber) {
+  switch (lampNumber) {
+      case LAMP_1:
+        LED_toggle(LAMP_1_PORT, LAMP_1_PIN);
+        break;
+      case LAMP_2:
+        LED_toggle(LAMP_2_PORT, LAMP_2_PIN);
+        break;
+      case LAMP_3:
+        LED_toggle(LAMP_3_PORT, LAMP_3_PIN);
+        break;
+      case LAMP_4:
+        LED_toggle(LAMP_4_PORT, LAMP_4_PIN);
+        break;
+      case LAMP_5:
+        LED_toggle(LAMP_5_PORT, LAMP_5_PIN);
+        break;
+      case LAMP_6:
+        LED_toggle(LAMP_6_DIMMABLE_PORT, LAMP_6_DIMMABLE_PIN);
+        break;
+    }
+  }
+
+// Adjust the brightness of the dimmable lamp
+void Lamp_dimmable_set_brightness(double brightness) {
+  PWM_set_DC(LAMP_6_DIMMABLE_PWM_PIN, brightness);
+  }
--- Smart_Home/Smart_Home/Services/Lights/lights.h
+++ Smart_Home/Smart_Home/Services/Lights/lights.h
@@ -3,14 +3,54 @@
  *
  * Created: 4/14/2023 6:25:27 AM
  *  Author: hesha
- */ 
+ */
 
 
 #ifndef LIGHTS_H_
 #define LIGHTS_H_
 
+#include "../../HAL/LED/led.h"
+#include "../../MCAL/Timer/timer.h"
 
+#define LAMP_1 1
+#define LAMP_2 2
+#define LAMP_3 3
+#define LAMP_4 4
+#define LAMP_5 5
+#define LAMP_6 6
 
+#define LAMP_1_PIN   2
+#define LAMP_1_PORT  PORT_C 
 
+#define LAMP_2_PIN   3
+#define LAMP_2_PORT  PORT_C 
+
+#define LAMP_3_PIN   4
+#define LAMP_3_PORT  PORT_C 
+
+#define LAMP_4_PIN   5
+#define LAMP_4_PORT  PORT_C 
+
+#define LAMP_5_PIN   6
+#define LAMP_5_PORT  PORT_C 
+
+#define LAMP_6_DIMMABLE_PIN      7
+#define LAMP_6_DIMMABLE_PWM_PIN  OC_2
+#define LAMP_6_DIMMABLE_PORT     PORT_D
+
+ // Initializes all six lamps
+void Lamps_init(void);
+
+// Turn on a lamp
+void Lamp_on(u8 lampNumber);
+
+// Turn off a lamp
+void Lamp_off(u8 lampNumber);
+
+// Toggle a lamp
+void Lamp_toggle(u8 lampNumber);
+
+// Adjust the brightness of the dimmable lamp
+void Lamp_dimmable_set_brightness(double brightness);
 
 #endif /* LIGHTS_H_ */
