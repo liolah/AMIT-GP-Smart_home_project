a88bde863616baa3526b7c5ce68b8fa9c81b8ff5
Hesham Hany <Hesham206645@bue.edu.eg>
2023-04-15 12:28:06 +0200

    Update servo.c

--- Smart_Home/Smart_Home/HAL/Servo/servo.c
+++ Smart_Home/Smart_Home/HAL/Servo/servo.c
@@ -8,13 +8,13 @@
 #include "servo.h"
 
  // Initialized the servo
-void servo_init() {
+void Servo_init() {
   DIO_init(SERVO_PIN, SERVO_PORT, OUT);
   PWM_init(SERVO_PWM_PIN, (SERVO_MIN_HIGH_PULSE_DURATION_mS / SERVO_PULSE_PERIODIC_TIME_mS), PWM_PHASE_CORRECT);
   Timer_start(TIMER_1, 8);
   }
 
 // Moves the servo to a specific angle (relative to the 0 angle position, not the previous one).
-void servo_move_to_angle(u8 angle) {
+void Servo_move_to_angle(u8 angle) {
   PWM_set_DC(SERVO_PWM_PIN, ((double)angle / ((SERVO_MAX_ANGLE - SERVO_MIN_ANGLE) * (SERVO_PULSE_PERIODIC_TIME_mS / (SERVO_MAX_HIGH_PULSE_DURATION_mS - SERVO_MIN_HIGH_PULSE_DURATION_mS)))) + (SERVO_MIN_HIGH_PULSE_DURATION_mS / SERVO_PULSE_PERIODIC_TIME_mS));
   }
